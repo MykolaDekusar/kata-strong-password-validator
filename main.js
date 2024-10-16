75423d915eeffacd305f0fe5fc6e1f731d34a048
MykolaDekusar <ndekusar97@gmail.com>
2024-10-16 14:51:20 +0200

    feat: Added basic pasword check

--- main.js
+++ main.js
@@ -6,33 +6,35 @@ const number = document.getElementById("number");
 const special = document.getElementById("special");
 const password = [];
 campoPassword.addEventListener("keydown", (e) => {
+  const availableInput = /[a-zA-Z0-9~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]/;
+  const ignoredKeys = ["Control", "Tab", "CapsLock", "Shift", "Alt"];
+
+  if (ignoredKeys.includes(e.key)) {
+    // Ignore these keys and do nothing
+    return;
+  }
   if (e.key === "Backspace" && password.length >= 1) {
     password.pop();
-    passwordCheck(password);
-  } else {
-    if (
-      e.key !== "Backspace" &&
-      e.key !== "Alt" &&
-      e.key !== "Shift" &&
-      e.key !== "Control"
-    ) {
-      password.push(e.key);
-      passwordCheck(password);
-    }
+  } else if (availableInput.test(e.key) && e.key !== "Backspace") {
+    password.push(e.key);
   }
+
+  passwordCheck(password.join(""));
 });
 
 function passwordCheck(pass) {
   if (pass.length >= 9) minNumber.classList.add("success");
-  else if (password.length < 9) {
-    minNumber.classList.remove("success");
-  }
+  else minNumber.classList.remove("success");
 
-  const containsUpper = /^(?=.*[A-Z]).*$/;
+  const containsUpper = /[A-Z]/;
   if (containsUpper.test(pass)) upper.classList.add("success");
   else upper.classList.remove("success");
 
-  const containsNumber = /^(?=.*[0-9]).*$/;
+  const containsNumber = /[0-9]/;
   if (containsNumber.test(pass)) number.classList.add("success");
   else number.classList.remove("success");
+
+  const containsSpecial = /[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]/;
+  if (containsSpecial.test(pass)) special.classList.add("success");
+  else special.classList.remove("success");
 }
